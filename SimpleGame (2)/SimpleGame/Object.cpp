8fcd950026a5e44a2942ffe86961ef4c42619cc4
kjskajamajin <31953442+kjskajamajin@users.noreply.github.com>
2017-10-12 17:29:03 +0900

    Add files via upload

---
+++ SimpleGame (2)/SimpleGame/Object.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "Object.h"
+#include "Renderer.h"
+
+Object::Object() : x(0), y(0), z(0), size(10), r(0), g(0), b(0), a(0)
+{
+
+}
+
+
+Object::~Object()
+{
+}
---
+++ SimpleGame (2)/SimpleGame/Object.h
@@ -0,0 +1,12 @@
+#pragma once
+class Object
+{
+private:
+public:
+	float x, y, z, r, g, b, size, a;
+	Object();
+	~Object();
+
+	void DrawRect();
+};
+
